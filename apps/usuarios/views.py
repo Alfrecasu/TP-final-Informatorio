cf27f8afa6929136f0f6f9d7a32d14e7752c224c
Alfrecasu <na.casullo@gmail.com>
2023-12-13 21:21:23 -0300

    ultimos cambios

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from .models import Usuarios
+from .forms import RegistrarUsuariosForm
+from django.urls import reverse_lazy
 # Create your views here.
+class RegistrarUsuario(CreateView):
+    model = Usuarios
+    form_class = RegistrarUsuariosForm
+    template_name = 'usuarios/registrar_usuarios.html'
+    successs_url = reverse_lazy('inicio')
+
